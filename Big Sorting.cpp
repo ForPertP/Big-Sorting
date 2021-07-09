0a07512165bea2f3ad1ebaa8812a36742295860f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-07-10 00:37:36 +0900

    Update Big Sorting.cpp

--- Big Sorting.cpp
+++ Big Sorting.cpp
@@ -1,1 +1,18 @@
-
+vector<string> bigSorting(vector<string> unsorted)
+{
+    sort(unsorted.begin(),unsorted.end(),
+        [](const string &i, const string &j) -> bool
+        {
+            int n = i.length();
+            int m = j.length();
+            if (n == m)
+            {
+               return (i < j);
+            }
+            
+            return n < m;
+        }
+    );
+    
+    return unsorted;
+}
