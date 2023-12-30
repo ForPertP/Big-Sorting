a53bffc435b24eaedcd32a1c672873d226d6332d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-12-31 04:15:26 +0900

    Update Big Sorting.cpp

--- Big Sorting.cpp
+++ Big Sorting.cpp
@@ -1,3 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'bigSorting' function below.
+ *
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts STRING_ARRAY unsorted as parameter.
+ */
+
 vector<string> bigSorting(vector<string> unsorted)
 {
     sort(unsorted.begin(),unsorted.end(),
